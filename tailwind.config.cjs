d0bbcab2288912b0eccfa862536e691af891ab28
Lucas Santos <leos.developer@gmail.com>
2023-02-09 00:58:57 -0300

    add classes customs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,24 @@ module.exports = {
   darkMode: "class",
   important: true,
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "button-calculator":
+          "0px 11px 7px rgba(0, 0, 0, 0.01), 0px 7px 7px rgba(0, 0, 0, 0.04), 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 2px 4px rgba(0, 0, 0, 0.26), 0px 0px 2px rgba(0, 0, 0, 0.29), inset 0px 2px 3px rgba(255, 255, 255, 0.06);",
+        "body-calculator":
+          "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 188px 52px rgba(0, 0, 0, 0.01), 0px 120px 48px rgba(0, 0, 0, 0.04), 0px 68px 41px rgba(0, 0, 0, 0.15), 0px 30px 30px rgba(0, 0, 0, 0.26), 0px 8px 17px rgba(0, 0, 0, 0.29), inset 0px 6px 8px rgba(255, 255, 255, 0.1), inset 0px -4px 5px rgba(0, 0, 0, 0.22)",
+      },
+      colors: {
+        custom: {
+          zinc: {
+            400: "#7F45E2",
+            600: "#462878",
+            900: "#2D2A37",
+          },
+        },
+      },
+    },
+  },
   plugins: [],
 };
